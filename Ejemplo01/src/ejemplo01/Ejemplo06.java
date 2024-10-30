013d304776367b36551684d40712ca9a178ab821
Paulo Andres <pbustamante@utpl.edu.ec>
2024-10-30 12:10:20 -0500

    Actualizacion 5

--- Ejemplo01/src/ejemplo01/Ejemplo06.java
+++ Ejemplo01/src/ejemplo01/Ejemplo06.java
@@ -26,7 +26,7 @@ public class Ejemplo06 {
         // usamos %f para reemplazar los valore decimales
         // System.out.printf("%s\n\n%s\n\n%s\n", nombreEstudiante, 
         //        apellidoEstudiante, nacimiento);
-        System.out.printf("Nombre:%s\n\n%s\n\n%s\n", nombreEstudiante, 
+        System.out.printf("Nombre: %s\n\nApellido: %s\n\nNacimiento: %s\n", nombreEstudiante, 
                 apellidoEstudiante, nacimiento);
         
     }
